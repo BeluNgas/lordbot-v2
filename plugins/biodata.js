350fadf65f306eeeeff3b06f18198fb39a6d6ed3
BelluNgas <98247916+BeluNgas@users.noreply.github.com>
2022-03-02 12:51:56 +0800

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -33,7 +33,7 @@ let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?
         "descriptionCount": "999999999",
         "productImageCount": "1",
       },
-      "businessOwnerJid": "6288229683561@s.whatsapp.net",
+      "businessOwnerJid": "601136151330@s.whatsapp.net",
       "contextInfo": {
         "forwardingScore": 9999,
         "isForwarded": false
